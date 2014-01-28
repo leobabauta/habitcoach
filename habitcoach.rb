c2bc75eacef39c81d009d9a1a18c09481a99ba0e
leobabauta <leo.babauta@gmail.com>
2014-01-27 20:46:35 -0800

    created user class, refactored all functions for this

--- habitcoach.rb
+++ habitcoach.rb
@@ -4,49 +4,68 @@
 puts "Welcome to Habit Coach 2000."
 # Main program is at bottom, calling these functions below
 
+
+# Create User class
+class User
+	attr_accessor :state, :user_name
+
+	def initialize(state,user_name)
+		@state = state
+		@user_name = user_name
+	end
+
+	def setstate(setstate)
+		state = setstate
+		puts "State set to:" + state.to_s   # for testing only
+	end
+
+	def levelup
+		@state += 1
+	end
+
+end
+
 # Find out if first time here
-def userstate
+def check_first(current_user)
 	puts "Is this your first time here?"
-	user_state = gets.chomp
-	if user_state[0].downcase == "y"
-		user_state = 0
-	elsif user_state[0].downcase == "n"
-		user_state = 1
+	firsttime = gets.chomp
+	if firsttime[0].downcase == "y"
+		current_user.setstate(0)
+	elsif firsttime[0].downcase == "n"
+		current_user.setstate(1)
 	else
 		puts "A simple yes or no please."
-		userstate
+		check_first
 	end
-	return user_state
 end
 
 # Find out if they understand habits, or what stage they're in
-def stage
+def stage(current_user)
 	understand = gets.chomp
 	case understand[0]
 	when "n", "N"
-		user_state = 0
+		return
 	when "y", "Y", "1"
-		user_state = 1
+		current_user.levelup
 	when "2"
-		user_state = 2
+		current_user.setstate(2)
 	when "3"
-		user_state = 3
+		current_user.setstate(3)
 	when "4"
-		user_state = 4
+		current_user.setstate(4)
 	when "5"
-		user_state = 5
+		current_user.setstate(5)
 	else
 		puts "Sorry, I don't understand - can you try again?"
-		stage
+		stage(current_user)
 	end
-	return user_state
 end
 		
 # First: Greet you and explain what he does
 # Then ask about understanding habits (or where the user was before)
 # Test: Do you understand how habits work?
-def greet(user_state)
-	case user_state
+def greet(current_user)
+	case current_user.state
 	when 0		# Greeting for a new user
 		puts <<-PARAGRAPH
 Awesome. I'm Hal, an intelligent Habit Coach bot designed to help you create habits.
@@ -63,76 +82,82 @@ Welcome back! What stage were you in before?
 (1= set habit, 2= set trigger, 3= set motivation, 4= set reminders, 5= set feedback)
 		PARAGRAPH
 	end
-	stage
+	stage(current_user)
 end
 
 # Habit course teaches people about habits
-def habitcourse
+def habitcourse(current_user)
 	puts "habitcourse" # for testing
 
-	return user_state = 1
+	current_user.levelup
 end
 
 # Habit set helps the user choose a habit
 # Test: what's your habit?
-def habitset
+def habitset(current_user)
 	puts "habitset" # for testing
 	habit = ""
 
-	return habit
+	current_user.levelup
+	habit = answer
 end
 
 # Trigger set helps the user set a trigger
 # Test: what's your trigger?
-def triggerset
+def triggerset(current_user)
 	puts "triggerset" # for testing
 	trigger = ""
 
-	return trigger
+	current_user.levelup
+	trigger = answer
 end
 
 # Motivation set helps the user set motivation
 # Test: what's your motivation?
-def motivationset
+def motivationset(current_user)
 	puts "motivationset" # for testing
 	motivation = ""
 
-	return motivation
+
+	current_user.levelup
+	motivation = answer
 end
 
 # Reminder set helps the user set a reminder
 # Test: how will you remember?
-def reminderset
+def reminderset(current_user)
 	puts "reminderset" # for testing
 	reminder = ""
 
-	return reminder
+	current_user.levelup
+	reminder = answer
 end
 
 # Feedback set helps the user set feedback loops
 # Test: what feedback loop will you use?
-def feedback set
+def feedbackset(current_user)
 	puts "feedbackset" # for testing
 	feedback = ""
 
-	return feedback
+	current_user.levelup
+	feedback = answer
 end
 
 # Route user to correct stage
-def route(user_state)
-	case user_state
+def route(current_user)
+	case current_user.state
 	when 0
-		habitcourse
+		habitcourse(current_user)
 	when 1
-		habitset
+		habitset(current_user)
 	when 2
-		triggerset
+		triggerset(current_user)
 	when 3
-		motivationset
+		motivationset(current_user)
 	when 4
-		reminderset
+		reminderset(current_user)
 	when 5
-		feedbackset
+		feedbackset(current_user)
 	end
 end
 
@@ -147,10 +172,15 @@ end
 
 # Main Program
 # Run the program below by calling above methods
-user_state = userstate # First check if user has been here
-user_state = greet(user_state) # Then greet based on user_state
 
-puts user_state # This is for testing purposes only, so I can see user_state
+# create a new user; NOTE: need to add way to input username
+current_user = User.new(0,"Leo")
 
-# Need to do: create a router loop with this method call:
-route(user_state)
+check_first(current_user) # First check if user has been here
+
+greet(current_user) # Then greet based on current_user.state
+
+puts "State:" + current_user.state.to_s # This is for testing purposes only, so I can see current_user.state
+
+# NEED TO DO: create a router loop with this method call:
+route(current_user)
